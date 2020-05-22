a894552b8d0125b6191271533984b2e192b19dac
Mohamed Camara <mohamed.camara@lilo.org>
2020-05-22 18:22:49 +0200

    fin du header + gestion de date

--- src/components/_Shared/Header/index.js
+++ src/components/_Shared/Header/index.js
@@ -1,10 +1,57 @@
-import react from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 export default function Header() {
+  const laDate = new Date();
+
+  const jours = [
+    "Dimache",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ];
+
+  const mois = [
+    "Janvier",
+    "Fevrier",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ];
+
   return (
     <View>
-      <Text> Ici sera la date</Text>
+      <Text style={Styles.title}>
+        {" " +
+          jours[laDate.getDay()] +
+          " " +
+          laDate.getDate() +
+          " " +
+          mois[laDate.getMonth()]}
+      </Text>
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+});
